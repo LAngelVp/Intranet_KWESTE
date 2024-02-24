6252700f0be5c5c2b4c25f17aa218f0c26464d63
Luis Angel Vallejo Perez <angelvallejop96@gmail.com>
2024-02-23 22:51:56 -0600

    Inicializacion del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './componentes/Navigation-Bar/navigation-bar/navigation-bar.component';
+import { FooterComponent } from './componentes/Footer-Web/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
